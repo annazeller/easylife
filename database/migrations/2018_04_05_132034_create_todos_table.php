7f8e5340ab787539b953f69d2d460af5d88340cd
Pia Freilinger <pia.freilinger@gmx.de>
2018-04-06 12:05:05 +0200

    Datenbank Migration für ToDOs

---
+++ database/migrations/2018_04_05_132034_create_todos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToDosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('userId');
+            $table->string('title');
+            $table->string('description');
+            $table->string('location');
+            $table->integer('priority');
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('todos');
+    }
+}
